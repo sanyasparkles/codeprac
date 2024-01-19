7fc11a40eeb1b8a37bb4ff0eec404bf601ebd5d0
bunnyyy <sanya.badhe@gmail.com>
2024-01-18 17:59:08 -0800

    trying sstiuff

--- src/App.svelte
+++ src/App.svelte
@@ -1,18 +1,18 @@
 <script>
-  import svelteLogo from './assets/svelte.svg'
-  import viteLogo from '/vite.svg'
+  // import svelteLogo from './assets/svelte.svg'
+  // import viteLogo from '/vite.svg'
   import Counter from './lib/Counter.svelte'
 </script>
 
 <main>
-  <div>
+  <!-- <div>
     <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
       <img src={viteLogo} class="logo" alt="Vite Logo" />
     </a>
     <a href="https://svelte.dev" target="_blank" rel="noreferrer">
       <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
     </a>
-  </div>
+  </div> -->
   <h1>Vite + Svelte</h1>
 
   <div class="card">
